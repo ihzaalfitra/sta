33a16a0680a8d762a91bc5d2d86cfecf9bb29c3a
ihzaalfitra <47023818+ihzaalfitra@users.noreply.github.com>
2022-07-27 20:14:34 +0700

    Update conn.php

--- kelola/conn.php
+++ kelola/conn.php
@@ -3,8 +3,8 @@
 	$username = "u4587348_admsta";
 	$password = "";
 	$db = "";
-
-    $conn=mysqli_connect("$host","$username","$password","$db");
+	//I accidentally commit this file that connects to database. But the old password and db password is unusable since my hosting has changed anyway
+    	$conn=mysqli_connect("$host","$username","$password","$db");
 
 	$db = new PDO("mysql:host=".@$host.";dbname=".@$db.";charset=utf8mb4", $username, $password);
 	$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
